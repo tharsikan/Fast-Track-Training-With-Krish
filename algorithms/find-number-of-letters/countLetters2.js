596f711502a953e03e662190f6e2f6cb04fd8068
Tharsikan <tharsikan0@gmail.com>
2021-05-31 11:22:07 +0530

    count letters using charecterstic of map&obj

---
+++ algorithms/find-number-of-letters/countLetters2.js
@@ -0,0 +1,20 @@
+let sent = "I'm Studying These Days.";
+const wordArray = Array.from(sent.replace(/ /g,''));
+function counter1(wordArray){
+    const objAllRepition = {};
+    wordArray.forEach(c => {
+        objAllRepition[c] = (objAllRepition[c] || 0) +1;
+    });
+    return objAllRepition;
+}
+function counter2(wordArray){
+    const mapAllRepition = new Map();
+    wordArray.forEach(c => {
+        mapAllRepition.set(c, (mapAllRepition.get(c) || 0) +1);
+    });
+    return mapAllRepition;
+}
+
+
+console.log(counter1(wordArray));
+console.log(counter2(wordArray));
