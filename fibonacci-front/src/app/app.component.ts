e90e1fd9447f63595cf208f5749102d720a8afa5
Tharsikan <tharsikan0@gmail.com>
2021-06-02 21:28:04 +0530

    fibonacci updated

--- fibonacci-front/src/app/app.component.ts
+++ fibonacci-front/src/app/app.component.ts
@@ -12,19 +12,24 @@ export class AppComponent {
   result: string = '';
   // tslint:disable-next-line:typedef
   clickme() {
-    this.result = `${this.number} is ${this.isFibonacci(+this.number) ? 'fibonacii' : 'not a fibonacii'}`;
-    console.log(this.isFibonacci(+this.number));
-
+    this.result = `${this.number} is ${this.isFibonacci(+this.number) ? 'fibonacii' : 'not a fibonacii'} and ${this.number}'th fibonacci is ${this.fib(+this.number)}`;
   }
   // tslint:disable-next-line:typedef
   isPerfectSquare( x: number){
-    const s = Math.sqrt(x);
+    const s = Math.floor(Math.sqrt(x));
     return (s * s === x);
   }
 
   // tslint:disable-next-line:typedef
   isFibonacci( n: number){
-    return this.isPerfectSquare(5 * n * n + 4) ||
-           this.isPerfectSquare(5 * n * n - 4);
+    return (this.isPerfectSquare(5 * n * n + 4) || this.isPerfectSquare(5 * n * n - 4));
+  }
+
+  // tslint:disable-next-line:typedef
+  fib(n: number): number {
+    if (n <= 1) {
+        return n;
+    }
+    return this.fib(n - 1) + this.fib(n - 2);
   }
 }
