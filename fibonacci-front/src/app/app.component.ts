5e5ff82803455d6d66949741a57bb069bf898775
Tharsikan <tharsikan0@gmail.com>
2021-06-02 21:29:06 +0530

    deleted commands

--- fibonacci-front/src/app/app.component.ts
+++ fibonacci-front/src/app/app.component.ts
@@ -8,24 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'fibonacci-front';
   number!: number;
-  // tslint:disable-next-line:no-inferrable-types
+  
   result: string = '';
-  // tslint:disable-next-line:typedef
+
   clickme() {
     this.result = `${this.number} is ${this.isFibonacci(+this.number) ? 'fibonacii' : 'not a fibonacii'} and ${this.number}'th fibonacci is ${this.fib(+this.number)}`;
   }
-  // tslint:disable-next-line:typedef
+
   isPerfectSquare( x: number){
     const s = Math.floor(Math.sqrt(x));
     return (s * s === x);
   }
 
-  // tslint:disable-next-line:typedef
+
   isFibonacci( n: number){
     return (this.isPerfectSquare(5 * n * n + 4) || this.isPerfectSquare(5 * n * n - 4));
   }
 
-  // tslint:disable-next-line:typedef
+
   fib(n: number): number {
     if (n <= 1) {
         return n;
