2a5c70bc9172f56d0ad7ab43abf8fe96f308a37b
Tharsikan <tharsikan0@gmail.com>
2021-06-02 18:54:11 +0530

    fibonacci

--- fibonacci-front/src/app/app.component.ts
+++ fibonacci-front/src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fibonacci-front';
+  number!: number;
+  // tslint:disable-next-line:no-inferrable-types
+  result: string = '';
+  // tslint:disable-next-line:typedef
+  clickme() {
+    this.result = `${this.number} is ${this.isFibonacci(+this.number) ? 'fibonacii' : 'not a fibonacii'}`;
+    console.log(this.isFibonacci(+this.number));
+
+  }
+  // tslint:disable-next-line:typedef
+  isPerfectSquare( x: number){
+    const s = Math.sqrt(x);
+    return (s * s === x);
+  }
+
+  // tslint:disable-next-line:typedef
+  isFibonacci( n: number){
+    return this.isPerfectSquare(5 * n * n + 4) ||
+           this.isPerfectSquare(5 * n * n - 4);
+  }
 }
