177c873592d8f5e3cdca089df4a073bb984a1e5d
Tharsikan <tharsikan0@gmail.com>
2021-06-16 19:37:11 +0530

    Three Service l

--- ThreeServices/three-service-front/src/app/home/nth-large/nth-large.component.ts
+++ ThreeServices/three-service-front/src/app/home/nth-large/nth-large.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'ts-nth-large',
   templateUrl: './nth-large.component.html',
-  styleUrls: ['./nth-large.component.scss']
+  styleUrls: ['./nth-large.component.scss'],
 })
 export class NthLargeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  number!: number;
+  list!: string;
+  valid = true;
+  nthLargeSubmit(nthLargeForm: NgForm): void {
+    if ((this.list.match(/,/g) || []).length + 1 < this.number) {
+      this.valid = false;
+    }else{
+      // logic
+    }
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
